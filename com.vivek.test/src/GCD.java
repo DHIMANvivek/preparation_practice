e9897a4f967fc8e7ce1e3ec98a20b50e20db094f
DHIMANvivek <googlydhiman.4236@gmail.com>
2022-01-08 20:55:39 +0530

    infytq

--- com.vivek.test/src/GCD.java
+++ com.vivek.test/src/GCD.java
@@ -1,33 +1,29 @@
 
 public class GCD {
-	
-	int hcf(int a , int b) {
-		if(a==0) 
-			return b;
-		
-		if(b==0)
-			return a;
-		
-		if(a==b)
-			return a;
-		
-	if(a>b) 
-		return hcf(a-b,b);
-	return hcf(a,b-a);
-	
-		
-		
-	}
 
-	
+	int hcf(int a, int b) {
+		if (a == 0)
+			return b;
+
+		if (b == 0)
+			return a;
+
+		if (a == b)
+			return a;
+
+		if (a > b)
+			return hcf(a - b, b);
+		return hcf(a, b - a);
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		GCD s = new GCD();
-		
+
 		int num = s.hcf(98, 56);
-		
+
 		System.out.print(num);
 
 	}
